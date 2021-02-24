6732c0336b81188856274439118062d526155a58
Kestanee <furkan.kestane@hotmail.com>
2021-02-24 23:21:59 +0300

    ..

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi.." + musteri.Id +" "+ musteri.Adi+" "+musteri.Soyadi+" "+musteri.TcNo);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Musteri[] musteriSil = new Musteri[]{ musteri };
+            foreach (var mustSil in musteriSil)
+            {
+                Console.WriteLine("Müşteri Silindi!.. " + mustSil.Id +" "+ mustSil.Adi+" "+mustSil.Soyadi+" "+mustSil.TcNo);
+            }
+        }
+        public void Listele(Musteri musteri)
+        {
+            Musteri[] musteriler = new Musteri[] { musteri };
+            foreach (var must in musteriler)
+            {
+                Console.WriteLine("Müşteri Listelendi.. " + must.Id + " "+ must.Adi+" "+must.Soyadi+" "+must.TcNo);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Furkan";
+            musteri1.Soyadi = "Kestane";
+            musteri1.TcNo = "11111111111";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Gökhan";
+            musteri2.Soyadi = "Kestane";
+            musteri2.TcNo = "11111111111";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+
+        }
+    }
+}
